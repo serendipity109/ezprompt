b7e3457ed3f6057aed2b85c9ee94b9bbb81e6be8
adamwang <adamwang@emotibot.com>
2023-04-28 18:25:50 +0800

    add routes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Home from './components/HomePage.vue'
+import Page from './components/PromptPage.vue'
 import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/page', component: Page }
+  ]
+})
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+})
