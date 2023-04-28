9929a2af0281b53986a626a03102d5590bed54e2
adamwang <adamwang@emotibot.com>
2023-04-28 17:19:05 +0800

    add align images

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -98,7 +98,8 @@
     </div>
     <div class="mb-[56px] sm:mb-0 sm:mt-[56px]">
       <div class="w-screen overflow-x-hidden flex flex-col bg-zinc-800 text-gray-100 text-sm">
-        <div class="flex flex-col items-center py-4 mt-16"><a class="font-semibold text-3xl text-gray-100" href="/">SDXL</a>
+        <div class="flex flex-col items-center py-4 mt-16"><a class="font-semibold text-3xl text-gray-100"
+            href="/">SDXL</a>
           <div class="flex items-center w-full max-w-[600px] md:ml-[48px] mt-8 px-4 pl-5 md:px-5">
             <div class="w-full">
               <div class="w-full flex items-center relative"><svg stroke="currentColor" fill="none" stroke-width="2"
@@ -109,14 +110,16 @@
                 </svg><input id="main-search" autoComplete="off" v-model="keyword" type="text"
                   class="bg-zinc-700 flex-1 pl-12 pr-12 rounded-full text-sm px-4 py-2.5 focus:outline-none focus:ring-1 focus:ring-indigo-700"
                   placeholder="Search for an image" value="" />
-                  <button class="text-base absolute right-2 hover:bg-zinc-800 h-8 w-8 flex items-center justify-center rounded-full" data-state="closed">
-                  <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
-                    stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em"
-                    xmlns="http://www.w3.org/2000/svg">
+                <button
+                  class="text-base absolute right-2 hover:bg-zinc-800 h-8 w-8 flex items-center justify-center rounded-full"
+                  data-state="closed">
+                  <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
+                    stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
                     <circle cx="8.5" cy="8.5" r="1.5"></circle>
                     <polyline points="21 15 16 10 5 21"></polyline>
-                  </svg></button></div>
+                  </svg></button>
+              </div>
             </div>
             <div class="flex justify-center"><button
                 class="ml-2 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center  bg-transparent hover:bg-zinc-900"><svg
@@ -136,9 +139,11 @@
           <div class="flex w-full max-w-[600px] md:ml-[48px] px-4 pl-5 md:px-5"></div>
           <div class=" mb-8 flex flex-col items-center">
             <div class="flex space-x-2">
-              <button @click="sendRequest" class="w-32 sm:w-36 flex items-center text-xs justify-center text-center  h-9 rounded-full  hover:brightness-110 bg-opacity-0 shadow-sm  mt-4 bg-gradient-to-t from-indigo-900 via-indigo-900 to-indigo-800">Search</button>
+              <button @click="sendRequest"
+                class="w-32 sm:w-36 flex items-center text-xs justify-center text-center  h-9 rounded-full  hover:brightness-110 bg-opacity-0 shadow-sm  mt-4 bg-gradient-to-t from-indigo-900 via-indigo-900 to-indigo-800">Search</button>
               <a href="/aperture">
-                <button class="w-32 sm:w-36 flex items-center text-xs justify-center text-center  h-9 rounded-full  hover:brightness-110 bg-opacity-0 shadow-sm  mt-4 border border-gray-700 hover:bg-zinc-700">Generate</button>
+                <button
+                  class="w-32 sm:w-36 flex items-center text-xs justify-center text-center  h-9 rounded-full  hover:brightness-110 bg-opacity-0 shadow-sm  mt-4 border border-gray-700 hover:bg-zinc-700">Generate</button>
               </a>
             </div>
           </div>
@@ -146,14 +151,61 @@
           <div class="w-full mt-4 px-1 relative"></div>
         </div>
       </div>
-    <div w-full mt-4 px-1 relative>
-      <div role="grid" class="active:outline-none focus:outline-none" tabindex="0" style="position: relative; width: 100%; max-width: 100%; height: 6053px; max-height: 6053px;">
-        <div v-for="(imageUrl, index) in imageUrls" :key="index">
-        <img :src="imageUrl" :style="{ width: imageWidth + 'px', height: imageHeight + 'px' }"/>
-      </div>
+      <div w-full mt-4 px-1 relative>
+        <div role="grid" class="w-screen overflow-x-hidden flex flex-col bg-zinc-800 text-gray-100 text-sm" tabindex="0"
+          style="position: relative; width: 100%; max-width: 100%; height: 6053px; max-height: 6053px;">
+          <div class="image-row" v-for="(row, index) in imageRows" :key="index">
+            <div class="image-container" v-for="(image, index) in row" :key="index">
+              <router-link to="/pagination" class="block relative group select-none overflow-hidden m-0.5 border-indigo-600  rounded-xl"
+                style="transition: opacity 500ms ease 0s;">
+                <div
+                  class="absolute inset-0 z-10 block text-zinc-100 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none line-clamp px-2 pb-2 text-sm px-2"
+                  style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.4) 40%, rgba(0, 0, 0, 0.1) 100%);">
+                  <div class="flex-shrink h-full flex items-end">
+                    <div class="flex flex-col">
+                      <p class="text-sm mb-1.5 font-medium leading-snug"
+                        style="overflow: hidden; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">
+                        Photography of cyborg pilot Russian</p>
+                      <p class="opacity-60 leading-snug text-sm"
+                        style="overflow: hidden; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">
+                        Interior cockpit, hyperdetailed, by wes anderson, pastel colors,...</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="top-2 left-0 absolute w-full flex z-10 text-zinc-100 justify-between opacity-0 group-hover:opacity-100 transition-opacity px-2 mb-1 text-sm">
+                  <button
+                    class=" bg-zinc-900 bg-opacity-50 hover:bg-opacity-100 transition-opacity flex items-center justify-center cursor-pointer text-lg h-10 w-10 rounded-lg">
+                    <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
+                      stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                      <circle cx="11" cy="11" r="8"></circle>
+                      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                  </button>
+                  <div class="flex flex-col space-y-2">
+                    <button
+                      class="  bg-zinc-900 bg-opacity-70 hover:bg-opacity-100 flex items-center justify-center cursor-pointer active:scale-90 transition-all  rounded-lg text-lg h-10 w-10">
+                      <div class="scale-50">
+                        <div class="heart brightness-110"></div>
+                      </div>
+                    </button>
+                    <button
+                      class="bg-zinc-900 bg-opacity-50 hover:bg-opacity-100 transition-opacity flex items-center justify-center cursor-pointer text-lg h-10 w-10 rounded-lg"><svg
+                        stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em"
+                        width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M224 96l16-32 32-16-32-16-16-32-16 32-32 16 32 16 16 32zM80 160l26.66-53.33L160 80l-53.34-26.67L80 0 53.34 53.33 0 80l53.34 26.67L80 160zm352 128l-26.66 53.33L352 368l53.34 26.67L432 448l26.66-53.33L512 368l-53.34-26.67L432 288zm70.62-193.77L417.77 9.38C411.53 3.12 403.34 0 395.15 0c-8.19 0-16.38 3.12-22.63 9.38L9.38 372.52c-12.5 12.5-12.5 32.76 0 45.25l84.85 84.85c6.25 6.25 14.44 9.37 22.62 9.37 8.19 0 16.38-3.12 22.63-9.37l363.14-363.15c12.5-12.48 12.5-32.75 0-45.24zM359.45 203.46l-50.91-50.91 86.6-86.6 50.91 50.91-86.6 86.6z">
+                        </path>
+                      </svg></button>
+                  </div>
+                </div>
+                <img v-bind:src="image" class="img-responsive" alt="Responsive Image">
+              </router-link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -165,7 +217,8 @@ export default {
     return {
       message: "",
       keyword: "",
-      imageUrls: [],
+      images: [],
+      imageRows: [],
     };
   },
   async created() {
@@ -178,14 +231,48 @@ export default {
   methods: {
     async sendRequest() {
       const response = await axios.get('http://192.168.3.16:8877/get_images',
-      {
-        params: {
-          keyword: this.keyword,
-        }
-      });
-      this.imageUrls = response.data;
+        {
+          params: {
+            keyword: this.keyword,
+          }
+        });
+      this.images = response.data;
       console.log(response.data);
+      this.imageRows = this.chunkArray(this.images, 5);
     },
+    chunkArray(array, size) {
+      const chunkedArray = [];
+      for (let i = 0; i < array.length; i += size) {
+        chunkedArray.push(array.slice(i, i + size));
+      }
+      return chunkedArray;
+    }
   },
 };
 </script>
+
+<style>.image-gallery {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-container {
+  margin: 0px;
+  flex: 1;
+  max-width: 20%;
+  height: auto;
+  text-align: center;
+}
+
+.img-responsive {
+  max-width: 100%;
+  height: auto;
+}</style>
