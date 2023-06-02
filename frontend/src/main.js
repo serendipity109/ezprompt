41897290c7a60a81c41a06decba78bd0a906f8be
adamwang <adamwang@emotibot.com>
2023-06-02 18:22:09 +0800

    add google auth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
 import { createVuetify } from "vuetify";
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import router from './router'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import router from "./router";
+import vue3GoogleLogin from 'vue3-google-login'
 
 const vuetify = createVuetify({});
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title // 設置網頁標題
-  next()
-})
+  document.title = to.meta.title; // 設置網頁標題
+  next();
+});
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .use(ElementPlus)
-  .mount('#app')
+// create your Vue app
+const app = createApp(App);
+
+app.use(vue3GoogleLogin, {
+  clientId: '734101254265-37fk1rd4de279jecj99m6l78pq8rqljh.apps.googleusercontent.com'
+});
+
+app.use(router).use(vuetify).use(ElementPlus).mount("#app");
---
+++ frontend/src/views/GoogleAuth.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { decodeCredential } from 'vue3-google-login'
+const callback = (response) => {
+  // decodeCredential will retrive the JWT payload from the credential
+  console.log("Handle the response", response)
+  const userData = decodeCredential(response.credential)
+  console.log("Handle the userData", userData)
+}
+</script>
+
+<template>
+  <GoogleLogin :callback="callback" auto-login/>
+</template>
