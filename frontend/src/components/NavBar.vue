5b0e416f31d347fe134ffc434cf55467e6daa622
adamwang <adamwang@emotibot.com>
2023-06-20 15:37:49 +0800

    add fix unittest bug

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -140,20 +140,3 @@ export default defineComponent({
     },
 });
 </script>
-<style scoped>
-.login-button {
-  color: white;
-}
-
-.modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-</style>
