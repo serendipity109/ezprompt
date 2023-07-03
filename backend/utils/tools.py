a845742057fe985f44f7c0df96088387f3e8a28f
adamwang <adamwang@emotibot.com>
2023-07-03 15:28:54 +0800

    fix re error

--- backend/utils/tools.py
+++ backend/utils/tools.py
@@ -50,7 +50,7 @@ async def schema_validator(websocket):
                 msg = {"code": 200, "message": "Image url is valid!", "result": ""}
                 await websocket.send_text(json.dumps(msg))
     if "size" in data.keys():
-        pattern = "^\d+:\d+$"
+        pattern = r"^\d+:\d+$"
         size = data["size"]
         if isinstance(size, str) and re.match(pattern, size):
             pass
